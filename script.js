968801447b5953d836fa74eba97abf8c4d6e38ab
Amol shelke <shelkeamol897@gmail.com>
2022-09-12 21:43:06 +0530

    Selected Elements

---
+++ script.js
@@ -0,0 +1,7 @@
+const balance = document.getElementById("balance");
+const moneyPlus = document.getElementById("money-plus");
+const moneyMinus = document.getElementById("money-minus");
+const list = document.getElementById("list");
+const form = document.getElementById("form");
+const text = document.getElementById("text");
+const amount = document.getElementById("amount");
