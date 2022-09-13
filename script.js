2b6d355208d6f6b956f38383233252b5e61f08ad
Amol shelke <shelkeamol897@gmail.com>
2022-09-13 17:37:23 +0530

    Added getInputFrom user function

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ const list = document.getElementById("list");
 const form = document.getElementById("form");
 const text = document.getElementById("text");
 const amount = document.getElementById("amount");
+
+let totalBalance = 0;
+
+const getInputFromUser = function (title, amount) {
+  if (amount < 0) {
+    console.log(`${title}, Expense ${amount}`);
+  } else {
+    totalBalance += amount;
+    console.log(`${title}, Income ${amount}`);
+  }
+  console.log(totalBalance);
+};
+
+getInputFromUser("Salary", 100000);
