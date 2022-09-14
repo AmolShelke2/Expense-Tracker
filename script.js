d4ebb1a5139eaa23635c9c8c91fe4d660d6cac81
Amol shelke <shelkeamol897@gmail.com>
2022-09-14 19:19:12 +0530

    Added AddTransiction function

--- script.js
+++ script.js
@@ -5,3 +5,38 @@ const list = document.getElementById("list");
 const form = document.getElementById("form");
 const text = document.getElementById("text");
 const amount = document.getElementById("amount");
+
+// Add Transaction
+
+const loacalStorageTransactions = JSON.parse(
+  localStorage.getItem("transactions")
+);
+
+let transactions =
+  localStorage.getItem("transactions") !== null
+    ? loacalStorageTransactions
+    : [];
+
+function addTransaction(e) {
+  e.preventDefault();
+
+  if (text.ariaValueMax.trim() === "" || amount.value.trim() === "") {
+    alert("Please add a text and amount");
+  } else {
+    const transaction = {
+      id: genrateID(),
+      text: text.value,
+      amount: +amount.value,
+    };
+
+    transactions.push(transaction);
+
+    text.value = "";
+    amount.value = "";
+  }
+}
+
+// Generate Randome ID
+function genrateID() {
+  return Math.floor(Math.random() * 1000000000);
+}
