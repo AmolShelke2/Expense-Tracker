800eee3896b79ec2c0826abc35b53f0ad445a484
Amol shelke <shelkeamol897@gmail.com>
2022-09-14 17:59:23 +0530

    Removed getInputFromUser function and adding dummy transiction

--- script.js
+++ script.js
@@ -5,17 +5,3 @@ const list = document.getElementById("list");
 const form = document.getElementById("form");
 const text = document.getElementById("text");
 const amount = document.getElementById("amount");
-
-let totalBalance = 0;
-
-const getInputFromUser = function (title, amount) {
-  if (amount < 0) {
-    console.log(`${title}, Expense ${amount}`);
-  } else {
-    totalBalance += amount;
-    console.log(`${title}, Income ${amount}`);
-  }
-  console.log(totalBalance);
-};
-
-getInputFromUser("Salary", 100000);
